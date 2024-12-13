babf301e3c7de25fc47f50eec493ac9d5eb34a17
Divyansh Rai <divyanshrai27@gmail.com>
2024-12-13 21:33:50 +0530

    refactor WeatherCard icon handling to use a mapping object for improved maintainability

--- src/Components/WeatherCard.jsx
+++ src/Components/WeatherCard.jsx
@@ -1,14 +1,25 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState } from 'react'
-import { useDate } from '../Utils/useDate'
-import sun from '../assets/icons/sun.png'
-import cloud from '../assets/icons/cloud.png'
-import fog from '../assets/icons/fog.png'
-import rain from '../assets/icons/rain.png'
-import snow from '../assets/icons/snow.png'
-import storm from '../assets/icons/storm.png'
-import wind from '../assets/icons/windy.png'
-import '../index.css'
+import React, { useEffect, useState } from 'react';
+import { useDate } from '../Utils/useDate';
+import sun from '../assets/icons/sun.png';
+import cloud from '../assets/icons/cloud.png';
+import fog from '../assets/icons/fog.png';
+import rain from '../assets/icons/rain.png';
+import snow from '../assets/icons/snow.png';
+import storm from '../assets/icons/storm.png';
+import wind from '../assets/icons/windy.png';
+import '../index.css';
+
+// Mapping weather conditions to their respective icons
+const iconMap = {
+  cloud: cloud,
+  rain: rain,
+  clear: sun,
+  thunder: storm,
+  fog: fog,
+  snow: snow,
+  wind: wind,
+};
 
 const WeatherCard = ({
   temperature,
@@ -19,56 +30,41 @@ const WeatherCard = ({
   iconString,
   conditions,
 }) => {
-
-  const [icon, setIcon] = useState(sun)
-  const { time } = useDate()
+  const [icon, setIcon] = useState(sun);
+  const { time } = useDate();
 
   useEffect(() => {
     if (iconString) {
-      switch (true) {
-      case iconString.toLowerCase().includes('cloud'):
-        setIcon(cloud)
-        break
-      case iconString.toLowerCase().includes('rain'):
-        setIcon(rain)
-        break
-      case iconString.toLowerCase().includes('clear'):
-        setIcon(sun)
-        break
-      case iconString.toLowerCase().includes('thunder'):
-        setIcon(storm)
-        break
-      case iconString.toLowerCase().includes('fog'):
-        setIcon(fog)
-        break
-      case iconString.toLowerCase().includes('snow'):
-        setIcon(snow)
-        break
-      case iconString.toLowerCase().includes('wind'):
-        setIcon(wind)
-        break
-      default:
-        setIcon(sun)
-      }
+      const lowerCaseIconString = iconString.toLowerCase();
+      const matchedIcon = Object.keys(iconMap).find(key =>
+        lowerCaseIconString.includes(key)
+      );
+      setIcon(iconMap[matchedIcon] || sun);
     }
-  }, [iconString])
+  }, [iconString]);
 
   return (
     <div className='w-[22rem] min-w-[22rem] h-[30rem] glassCard p-4'>
-      <div className='flex w-full just-center, items-center gap-4 mt-12 mb-4'>
+      <div className='flex w-full just-center items-center gap-4 mt-12 mb-4'>
         <img src={icon} alt="weather_icon" />
-        <p className='font-bold text-5xl flex justify-center items-center' >{temperature} &deg;C</p>
+        <p className='font-bold text-5xl flex justify-center items-center'>
+          {temperature} &deg;C
+        </p>
       </div>
-      <div className='font-bold text-center text-xl'>
-        {place}
-      </div>
+      <div className='font-bold text-center text-xl'>{place}</div>
       <div className='w-full flex justify-between items-center mt-4'>
         <p className='flex-1 text-center p-2'>{new Date().toDateString()}</p>
         <p className='flex-1 text-center p-2'>{time}</p>
       </div>
       <div className='w-full flex justify-between items-center mt-4 gap-4'>
-        <p className='flex-1 text-center p-2 font-bold bg-blue-600 shadow rounded-lg'>Wind Speed <p className='font-normal'>{windspeed} km/h</p></p>
-        <p className='flex-1 text-center p-2 font-bold rounded-lg bg-green-600'>Humidity <p className='font-normal'>{humidity} gm/m&#179;</p></p>
+        <div className='flex-1 text-center p-2 font-bold bg-blue-600 shadow rounded-lg'>
+          Wind Speed
+          <p className='font-normal'>{windspeed} km/h</p>
+        </div>
+        <div className='flex-1 text-center p-2 font-bold rounded-lg bg-green-600'>
+          Humidity
+          <p className='font-normal'>{humidity} gm/m&#179;</p>
+        </div>
       </div>
       <div className='w-full p-3 mt-4 flex justify-between items-center'>
         <p className='font-semibold text-lg'>Heat Index</p>
@@ -79,7 +75,7 @@ const WeatherCard = ({
         {conditions}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WeatherCard
+export default WeatherCard;
