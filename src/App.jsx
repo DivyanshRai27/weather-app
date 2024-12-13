7121f3845251e174aaa52c651166ce4db736ce9a
Divyansh Rai <divyanshrai27@gmail.com>
2024-12-13 21:54:43 +0530

    add search input and background layout to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { useState } from 'react'
 import './App.css'
+import search from './assets/icons/search.svg'
+import { BackgroundLayout } from './Components'
 
 function App() {
 
+  const [input, setInput] = useState('')
+  // console.log(weather)
+
+ 
+
   return (
-    <div className='text-xl font-bold text-center'>
-      Hello World
-      
+    <div className='w-full h-screen text-white px-8'>
+      <nav className='w-full p-3 flex justify-between items-center'>
+        <h1 className='font-bold tracking-wide text-3xl'>Weather App</h1>
+        <div className='bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2'>
+          <img src={search} alt="search" className='w-[1.5rem] h-[1.5rem]' />
+          <input onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              // sumit the form
+              console.log(input)
+            }
+          }} type="text" placeholder='Search city' className='focus:outline-none w-full text-[#212121] text-lg' value={input} onChange={e => setInput(e.target.value)} />
+        </div>
+      </nav>
+      <BackgroundLayout></BackgroundLayout>
     </div>
   )
 }
