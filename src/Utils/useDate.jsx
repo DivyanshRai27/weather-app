44a50ebe6dd396372817b93199ccfc58278fef4b
Divyansh Rai <divyanshrai27@gmail.com>
2024-12-14 14:44:24 +0530

    refactor useDate hook

--- src/Utils/useDate.jsx
+++ src/Utils/useDate.jsx
@@ -9,23 +9,21 @@ import { useEffect, useState } from "react";
 
 export const useDate = () => {
     const locale = 'en';
-    const [today, setDate] = useState(new Date())
+    const [today, setDate] = useState(new Date());
 
     useEffect(() => {
         const timer = setInterval(() => {
-            setDate(new Date())
-        }, 60*1000)
+            setDate(new Date());
+        }, 60000);
 
         return () => {
-            clearInterval(timer)
-        }
-    },[])
+            clearInterval(timer);
+        };
+    }, []);
 
-    const day = today.toLocaleDateString(locale, {weekday: 'long'})
-    const date = `${day}, ${today.getDate()}, ${today.toLocaleDateString(locale, {month: 'long'})}\n\n`
-    const time = today.toLocaleDateString(locale, { hour: 'numeric', hour12: true, minute: 'numeric' })
+    const day = today.toLocaleDateString(locale, { weekday: 'long' });
+    const date = `${day}, ${today.getDate()} ${today.toLocaleDateString(locale, { month: 'long' })}`;
+    const time = today.toLocaleTimeString(locale, { hour: 'numeric', hour12: true, minute: 'numeric' });
 
-    return {
-        date, time
-    }
-}
+    return { date, time };
+};
